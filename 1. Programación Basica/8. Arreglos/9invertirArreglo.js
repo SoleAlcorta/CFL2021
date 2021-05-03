968ab3b5203f153e5da9911ba6454f8dae051f34
SoleAlcorta <soleatandil@hotmail.com>
2021-05-03 18:53:44 -0300

    Ejercicio

--- 1. Programación Basica/8. Arreglos/9invertirArreglo.js
+++ 1. Programación Basica/8. Arreglos/9invertirArreglo.js
@@ -3,12 +3,13 @@ let dimensionArray = readlineSync.questionInt("Ingrese la dimension del arreglo:
 let arrayUsuario = new Array(dimensionArray);
 let arrayInvertido = new Array(dimensionArray);
 let i=0;
+let indice=0;
 for (i=0; i<dimensionArray; i++) {
     arrayUsuario[i] = readlineSync.questionInt("Ingrese un numero: ");
 }
 while (dimensionArray >=0) {
-    arrayInvertido[i] = arrayUsuario[dimensionArray];
-    i=i+1;
+    arrayInvertido[indice] = arrayUsuario[dimensionArray];
+    indice=indice+1;
     dimensionArray = dimensionArray-1;
 }
 console.log(arrayUsuario);
